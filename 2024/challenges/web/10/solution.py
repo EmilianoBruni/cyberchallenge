6687446acf725094510e1690c02d6f78550a2c9b
ebruni <info@ebruni.it>
2024-04-09 12:18:00 +0200

    Converted to LF and add print comments

--- 2024/challenges/web/10/solution.py
+++ 2024/challenges/web/10/solution.py
@@ -13,19 +13,31 @@ def print_response(response):
 # Define the URL of the website you want to scrape
 url = 'http://web-10.challs.olicyber.it/'
 
+print("Let's see what methods are allowed on the server")
 res = requests.options(url)
 pprint(res.headers['Allow'])
 
+print("\nTry a valid method\n")
+print("GET request")
+res = requests.get(url)
+pprint(res.headers)
+
 # try post, put, delete, patch
+print("\nLet's try some methods\n")
+
+print("Try POST request")
 res = requests.post(url)
 pprint(res.headers);
 
+print("\nTry PUT request")
 res = requests.put(url)
 pprint(res.headers);
 
+print("\nTry DELETE request")
 res = requests.delete(url)
 pprint(res.headers);
 
+print("\nTry PATCH request")
 res = requests.patch(url)
 pprint(res.headers);
 
